3f83f8226d548447d79fa6dce72e9456ae67bf14
JdM <julien@rmbi.ch>
2025-01-19 16:17:04 +0100

    Refactor graph visualization to support fixed edge sizes and configurable edge display

--- presentation/main.py
+++ presentation/main.py
@@ -5,6 +5,10 @@ import igraph as ig
 from database import Database
 import matplotlib.pyplot as plt
 
+FIXED_EDGES_SIZE = False
+SHOW_EDGES = True
+
+
 db = Database("data.db")
 
 sites = db.get_sites()
@@ -15,36 +19,40 @@ g.add_vertices(len(sites))
 
 for i in range(len(sites)):
     g.vs[i]["name"] = sites[i][1]
-    g.vs[i]['size'] = len(db.get_links(to_link_id=sites[i][0])) * 10
+    power = len(db.get_links(to_link_id=sites[i][0]))
+    
+    g.vs[i]['size'] = 10 if FIXED_EDGES_SIZE else power*10
 
 edges = []
 
+if SHOW_EDGES:
+    for link in links:
 
-for link in links:
+        for y in range(len(sites)):
+            if sites[y][0] == link[1]:
+                site_from_index = y
 
-    for y in range(len(sites)):
-        if sites[y][0] == link[1]:
-            site_from_index = y
-
-        if sites[y][0] == link[2]:
-            site_to_index = y
+            if sites[y][0] == link[2]:
+                site_to_index = y
 
 
-    
-    edges.append((site_from_index, site_to_index))
+        
+        edges.append((site_from_index, site_to_index))
 
-g.add_edges(edges)
+    g.add_edges(edges)
+
 g.vs['label'] = g.vs['name']
-
  
 
 fig, ax = plt.subplots()
 ig.plot(
     g, 
     target=ax,
+    layout="graphopt",
+    edge_width=0.4,
     vertex_size=g.vs["size"],
     vertex_label=g.vs['label'],
     vertex_color='orange',
-    edge_color='black'
+    edge_color='grey',
 )
 plt.show()
